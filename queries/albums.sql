bd527a246f323c9ac8ba882e4a717093a262fb60
Anna Arnold <anna.arnold.96@gmail.com>
2024-10-08 15:42:27 +0100

    workshop day 2 extension complete

--- queries/albums.sql
+++ queries/albums.sql
@@ -2,6 +2,7 @@
 -- sqlite3 db/db.sqlite < queries/example.sql
 
 .mode json
-SELECT *
+SELECT albums.*, artists.name AS artist_name
 FROM albums
+INNER JOIN artists ON albums.artist_id = artists.id
 ORDER BY name;
