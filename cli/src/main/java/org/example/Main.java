682db65381dda9c9ccecf8cc89555525d23116c5
Anna Arnold <anna.arnold.96@gmail.com>
2024-10-08 14:19:01 +0100

    updated main

--- cli/src/main/java/org/example/Main.java
+++ cli/src/main/java/org/example/Main.java
@@ -41,7 +41,7 @@ public class Main {
 
 
 
-            System.out.println(userList);
+            System.out.println(userList.get(0).getEmail());
 
 
 //            List<String> test = new ArrayList<>();
